082820a6b44964d99981d3e0b82fd8f38cd92c24
Naing Linn Aung <100105059+nainglinn53@users.noreply.github.com>
2025-04-09 15:36:54 +0630

    Update scheduler_service.py

--- app/app/services/scheduler_service.py
+++ app/app/services/scheduler_service.py
@@ -16,9 +16,9 @@ from .celery_service import (send, send_keyword_by_task, send_source_by_task,
                              send_subtask)
 from .credentials_management import accounts_warming, proxy_re_enable
 
-TASK_PERCENTAGE = 90
+TASK_PERCENTAGE = 95
 SUBTASK_LIKE_PERCENTAGE = 0
-SUBTASK_COMMENT_PERCENTAGE = 10
+SUBTASK_COMMENT_PERCENTAGE = 5
 SUBTASK_SHARE_PERCENTAGE = 0
 SUBTASK_PERSONAL_DATA_PERCENTAGE = 0
 tl = Timeloop()
